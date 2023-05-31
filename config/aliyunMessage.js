8b4129ee438887c93a27bfbecaa21ae1fbf35436
simoon <420526391@qq.com>
2023-05-31 10:08:50 +0800

    发送验证码

---
+++ config/aliyunMessage.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+const AppCode = "9d1a02deaa57453e8d4aef10f8d1bb2c";
+
+const sendMsgCode = (phone, randomCode) => {
+  return axios({
+    method: "POST",
+    url: `https://jmsms.market.alicloudapi.com/sms/send?mobile=${phone}&templateId=JM1000372&value=${randomCode}`,
+    headers: {
+      authorization: `AppCode ${AppCode}`,
+    },
+  });
+};
+
+module.exports = {
+  sendMsgCode,
+};
